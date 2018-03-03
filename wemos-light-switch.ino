56ca203dedfbd56ac00c25c8e4baff81fde0baf4
kononowicz24 <07-001@wp.pl>
2018-03-03 21:05:15 +0100

    removing flashing the outputs

--- wemos-light-switch.ino
+++ wemos-light-switch.ino
@@ -13,16 +13,20 @@ void switchOutput(int a, int value) {
     case 0:
     case 1:
       switch (a) {
-        case 1: digitalWrite(D3, value); server.send (200, "text/html", "OK <a href='/1/on'>sw ON</a> <a href='/1/off'>sw OFF</a>"); break;
-        case 2: digitalWrite(D4, value); server.send (200, "text/html", "OK <a href='/2/on'>sw ON</a> <a href='/2/off'>sw OFF</a>"); break;
+        case 1: digitalWrite(D3, value);
+            server.send (200, "text/html", "OK <a href='/1/on'>sw ON</a> <a href='/1/off'>sw OFF</a>");
+            break;
+        case 2: digitalWrite(D4, value);
+            server.send (200, "text/html", "OK <a href='/2/on'>sw ON</a> <a href='/2/off'>sw OFF</a>");
+            break;
       }
       break;
-    case 2:
-      switch (a) {
-        case 1: digitalWrite(D3, value); delay(1000); digitalWrite(D3, !value);  server.send (200, "text/html", "OK"); break;
-        case 2: digitalWrite(D4, value); delay(1000); digitalWrite(D4, !value);  server.send (200, "text/html", "OK"); break;
-      }
-      break;
+    //case 2:
+    //  switch (a) {
+    //    case 1: digitalWrite(D3, value); delay(1000); digitalWrite(D3, !value);  server.send (200, "text/html", "OK"); break;
+    //    case 2: digitalWrite(D4, value); delay(1000); digitalWrite(D4, !value);  server.send (200, "text/html", "OK"); break;
+    //  }
+    //  break;
   }
  
 }
@@ -30,6 +34,7 @@ void switchOutput(int a, int value) {
 void setup ( void ) {
   pinMode (D3, OUTPUT);
   pinMode (D4, OUTPUT);
+  
 	Serial.begin ( 115200 );
 	WiFi.begin ( ssid, password );
 	Serial.println ( "" );
@@ -54,6 +59,7 @@ void setup ( void ) {
   server.on ("/2/on", []() {switchOutput(2,1);});
   server.on ("/1/off", []() {switchOutput(1,0);});
   server.on ("/1/on", []() {switchOutput(1,1);});
+  
 	server.begin();
 	Serial.println ( "HTTP server started" );
 }
