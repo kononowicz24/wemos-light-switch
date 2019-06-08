f79348422acb3d0f97a0181944e04bf10d51bff0
kononowicz24 <07-001@wp.pl>
2019-06-08 23:19:19 +0200

    fixed 8662944039f4ad597ca5044f43d87cd50d89fe7f conflict

--- wemos-light-switch.ino
+++ wemos-light-switch.ino
@@ -5,13 +5,9 @@
 
 #include "passwords.h" //this file contains the name and the password of the local router
 
-<<<<<<< HEAD
 ESP8266WebServer server ( 81 );
 bool out2state = false;
 bool button2state = false;
-=======
-ESP8266WebServer server ( 81 );//the port of the server would be 81
->>>>>>> 8662944039f4ad597ca5044f43d87cd50d89fe7f
 
 void switchOutput(int a, int value) {
   switch (value) {
@@ -32,15 +28,10 @@ void switchOutput(int a, int value) {
 }
 
 void setup ( void ) {
-<<<<<<< HEAD
   pinMode (D3, OUTPUT);
   pinMode (D4, OUTPUT);
   pinMode (D2, INPUT_PULLUP);
   pinMode (D1, OUTPUT);
-=======
-        pinMode (D3, OUTPUT);
-        pinMode (D4, OUTPUT);
->>>>>>> 8662944039f4ad597ca5044f43d87cd50d89fe7f
   
 	Serial.begin ( 115200 );//set serial terminal baud rate to 115200bps
 	WiFi.begin ( ssid, password );//start the wireless radio and connect it to the ssid
@@ -61,19 +52,10 @@ void setup ( void ) {
 		Serial.println ( "MDNS responder started" );
 	}
  
-<<<<<<< HEAD
   server.on ("/2/off", []() {switchOutput(2,0); out2state = false; });
   server.on ("/2/on", []() {switchOutput(2,1);  out2state = true; });
   server.on ("/1/off", []() {switchOutput(1,0);});
   server.on ("/1/on", []() {switchOutput(1,1);});
-=======
-	//on each connection to the server the function will be called depending on the address typed in
-        server.on ("/2/off", []() {switchOutput(2,0);}); //setting output 2 to 0=off
-        server.on ("/2/on", []() {switchOutput(2,1);}); // setting output 2 to 1=on
-        server.on ("/1/off", []() {switchOutput(1,0);});
-        server.on ("/1/on", []() {switchOutput(1,1);});
->>>>>>> 8662944039f4ad597ca5044f43d87cd50d89fe7f
-  
 	server.begin();
 	Serial.println ( "HTTP server started" );
 }
